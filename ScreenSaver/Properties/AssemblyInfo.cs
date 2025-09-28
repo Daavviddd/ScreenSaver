a02e533e362451f24ddfe0795f044eeb37bb4401
detko <detkov.david05@mail.ru>
2025-09-28 20:07:13 +0300

    Почти готово. Присутствует баг со снегопадом

---
+++ ScreenSaver/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ScreenSaver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScreenSaver")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("06a25982-8f42-4a4b-8e3e-e3da5e90d662")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ScreenSaver/Village.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class Village : Form
+    {
+        Point imagePoint;
+        List<Point> snowflakePoints = new List<Point>();
+        System.Windows.Forms.Timer timer;
+        Image snowflake;
+        Bitmap ResizeSnowflake;
+        int deltaY = 1;
+        Image scene;
+
+        List<bool> snowflakeActive;
+        List<int> snowflakeDelays;
+        int snowflakeCounter = 0;
+        Random random = new Random();
+        public Village()
+        {
+            InitializeComponent();
+            
+            this.BackgroundImage = Properties.Resources.Shrek;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.DoubleBuffered = true;
+
+            int newWindth = 60;
+            int newHight = 45;
+
+            int pointX = -1, pointY = -1;
+
+            snowflake = Properties.Resources.Snowflake;
+
+            ResizeSnowflake = new Bitmap(snowflake, new Size(newWindth, newHight));
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 100;
+
+            snowflakeActive = new List<bool>();
+            snowflakeDelays = new List<int>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                pointX = random.Next(0, ClientRectangle.Width);
+                pointY = random.Next(-500, -50);
+                imagePoint = new Point(pointX, pointY);
+
+                snowflakeActive.Add(false);
+                snowflakeDelays.Add(random.Next(0, 300));
+                snowflakePoints.Add(imagePoint);
+            }
+            this.Paint += Village_Paint_1;
+            timer.Start();
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            snowflakeCounter++;
+            for (int i = 0; i < snowflakePoints.Count; i++)
+            {
+                if (!snowflakeActive[i] && snowflakeCounter >= snowflakeDelays[i])
+                {
+                    snowflakeActive[i] = true;
+                }
+                if (snowflakeActive[i])
+                {
+                    Point point = snowflakePoints[i];
+                    point.Y += 2;
+                    snowflakePoints[i] = point;
+                    if (point.Y > ClientRectangle.Height)
+                    {
+                        point.Y = random.Next(-500, -50);
+                        point.X = random.Next(0, ClientRectangle.Width);
+                        snowflakePoints[i] = point;
+                    }
+                }
+            }
+            Invalidate();
+        }
+
+        private void Village_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Village_ResizeEnd(object sender, EventArgs e)
+        {
+            scene = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+        }
+
+        private void Village_Paint_1(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < snowflakePoints.Count; i++)
+            {
+                if(snowflakeActive[i])
+                {
+                    e.Graphics.DrawImage(ResizeSnowflake, snowflakePoints[i]);
+                }
+            }
+        }
+    }
+}
